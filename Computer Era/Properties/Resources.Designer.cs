3dfda92af0de4e6ecccbf8a15264d20c234f3f7e
Skvipers <skvipers@gmail.com>
2019-01-23 01:25:04 +0300

    menu button

--- Computer Era/Properties/Resources.Designer.cs
+++ Computer Era/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Computer_Era.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap start_button {
+            get {
+                object obj = ResourceManager.GetObject("start_button", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
