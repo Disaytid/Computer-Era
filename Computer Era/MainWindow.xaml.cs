d90b0cbb7dae1e330edff8fa6890af5142b69aed
Skvipers <skvipers@gmail.com>
2019-01-23 00:43:15 +0300

    init project

---
+++ Computer Era/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Computer_Era
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            GameMenu.Visibility = Visibility.Hidden;
+            CreateNewGame.Visibility = Visibility.Visible;
+        }
+
+        private void StartNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewGame.Visibility = Visibility.Hidden;
+            Game.Visibility = Visibility.Visible;
+            GamePanel.Visibility = Visibility.Visible;
+
+            ImageBrush brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Resources/agriculture.jpg"));
+            brush.Stretch = Stretch.UniformToFill;
+            this.Background = brush;
+        }
+    }
+}
