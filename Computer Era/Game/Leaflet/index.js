2a155d88539a43977c5c92acd42a7a8d42b22395
Skvipers <skvipers@gmail.com>
2019-01-31 23:31:29 +0300

    update card

--- Computer Era/Game/Leaflet/index.js
+++ Computer Era/Game/Leaflet/index.js
@@ -9,7 +9,13 @@
 var bankIcon = new BuildingIcon({ iconUrl: 'images/bank.png' }),
     evilTowerIcon = new BuildingIcon({ iconUrl: 'images/evil-tower.png' }),
     marketIcon = new BuildingIcon({ iconUrl: 'images/market.png' }),
-    microchipIcon = new BuildingIcon({ iconUrl: 'images/microchip.png' });
+    microchipIcon = new BuildingIcon({ iconUrl: 'images/microchip.png' }),
+    pcIcon = new BuildingIcon({ iconUrl: 'images/pc.png' }),
+    coffeeCupIcon = new BuildingIcon({ iconUrl: 'images/coffee-cup.png' }),
+    milkCartonIcon = new BuildingIcon({ iconUrl: 'images/milk-carton.png' }),
+    wheelbarrowIcon = new BuildingIcon({ iconUrl: 'images/wheelbarrow.png' }),
+    martiniIcon = new BuildingIcon({ iconUrl: 'images/martini.png' }),
+    bucketIcon = new BuildingIcon({ iconUrl: 'images/bucket.png' });
 
 function ReadState(options) {
     window.external.ReadState(options);
