9807a78080dca5d3d3ff2f18507ff0160011aa37
Skvipers <skvipers@gmail.com>
2019-02-04 00:14:00 +0300

    small fix

--- Computer Era/Game/Forms/LaborExchange.xaml.cs
+++ Computer Era/Game/Forms/LaborExchange.xaml.cs
@@ -136,7 +136,8 @@ namespace Computer_Era.Game.Forms
 
         private void CreateJobCards(Collection<Profession> profession, Collection<Company> companies, DateTime game_date)
         {
-            List<Company> current_companies = companies.Where(e => e.OpeningYear <= game_date).ToList();
+            List<Company> current_companies = companies.ToList();
+            current_companies.RemoveAll(e => e.OpeningYear.Year > game_date.Year);
 
             for (int i = 0; i < profession.Count; i++)
             {
