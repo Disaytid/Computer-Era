a2b18b1ed372f498374259cfaf8bba0580337086
Skvipers <skvipers@gmail.com>
2019-02-05 01:12:55 +0300

    edit class GameEvents

--- Computer Era/Game/GameEvents.cs
+++ Computer Era/Game/GameEvents.cs
@@ -10,30 +10,63 @@ using System.Windows.Threading;
 
 namespace Computer_Era.Game
 {
+    public delegate void MethodContainer();
 
     public class GameEvents
     {
         public List<GameEvent> Events = new List<GameEvent>();
-        public DateTime GameDate = new DateTime(1990, 1, 1, 7, 0, 0);
+        public DateTime GameDate = new DateTime(1990, 1, 1, 7, 0, 0); //после проверки удалить
 
         public GameTimer GameTimer = new GameTimer();
 
         public GameEvents()
         {
             //GameTimer.Week += Handler1.Message;
+
+            GameTimer.Hour += this.Hour;
+            GameTimer.Day += this.Day;
+
+            Events.Add(new GameEvent(1, "123", GameDate, Periodicity.Hour, 1, new MethodContainer(Test)));
+            Events.Add(new GameEvent(2, "456", GameDate, Periodicity.Day, 1, new MethodContainer(Test)));
+        }
+
+        private void Test()
+        {
+
+        }
+
+        private void Minute()
+        {
+
         }
 
         private void Hour()
         {
-
+            foreach (GameEvent game_event in Events.Where<GameEvent>(e => e.Periodicity == Periodicity.Hour).ToList())
+            {
+                //MessageBox.Show("Прошел час у " + game_event.Name);
+            }
         }
 
         private void Day()
         {
-
+            foreach (GameEvent game_event in Events.Where<GameEvent>(e => e.Periodicity == Periodicity.Day).ToList())
+            {
+                MessageBox.Show("Прошел день у " + game_event.Name);
+            }
         }
 
         private void Week()
+        {
+
+        }
+
+        private void Month()
+        {
+
+        }
+
+        private void Year()
         {
 
         }
@@ -57,9 +90,9 @@ namespace Computer_Era.Game
         DateTime ResponseTime;
         public Periodicity Periodicity;
         public int PeriodicityValue;
-        Func<DataTable> Method;
+        public MethodContainer Method;
 
-        public GameEvent(int id, string name, DateTime response_time, Periodicity periodicity, int periodicity_value, Func<DataTable> method)
+        public GameEvent(int id, string name, DateTime response_time, Periodicity periodicity, int periodicity_value, MethodContainer method)
         {
             Id = id;
             Name = name;
@@ -76,12 +109,19 @@ namespace Computer_Era.Game
         public DispatcherTimer Timer = new DispatcherTimer();
         private DateTime OldDateAndTime;
 
+        private int OldMinute;
+        private int OldHour;
+        private int OldDay;
+        private int OldMonth;
+
         public delegate void MethodContainer();
 
         public event MethodContainer Minute;
         public event MethodContainer Hour;
         public event MethodContainer Day;
         public event MethodContainer Week;
+        public event MethodContainer Month;
+        public event MethodContainer Year;
 
         public GameTimer()
         {
@@ -89,30 +129,49 @@ namespace Computer_Era.Game
             Timer.Interval = new TimeSpan(0, 0, 0, 0, 40);
 
             OldDateAndTime = DateAndTime;
+
+            OldMinute = DateAndTime.Minute;
+            OldHour = DateAndTime.Hour;
+            OldDay = DateAndTime.Day;
+            OldMonth = DateAndTime.Month;
         }
 
         public void TimerTick(object sender, EventArgs args)
         {
             DateAndTime = DateAndTime.AddMinutes(1);
 
-            if (DateAndTime.Minute > OldDateAndTime.Minute)
+            //Переписать все проверки они не верны
+            if (DateAndTime.Minute > OldMinute)
             {
-                OldDateAndTime = OldDateAndTime.AddMinutes((DateAndTime - OldDateAndTime).TotalMinutes);
+                OldMinute += DateAndTime.Minute - OldMinute;
+                if (OldMinute == 60) { OldMinute = 0;  }
                 Minute?.Invoke();
             }
-            if (DateAndTime.Hour > OldDateAndTime.Hour)
+            if (DateAndTime.Hour > OldHour & DateAndTime.Day == OldDay || DateAndTime.Hour < OldHour & DateAndTime.Day > OldDay)
             {
-                OldDateAndTime = OldDateAndTime.AddHours((DateAndTime - OldDateAndTime).TotalHours);
+                OldHour += DateAndTime.Hour - OldHour;
+                if (OldHour == 24) { OldHour = 0; }
                 Hour?.Invoke();
             }
-            if (DateAndTime.Day > OldDateAndTime.Day)
+            if (DateAndTime.Day > OldDay & DateAndTime.Month == OldMonth || DateAndTime.Day < OldDay & DateAndTime.Month > OldMonth)
             {
-                OldDateAndTime = OldDateAndTime.AddDays((DateAndTime - OldDateAndTime).TotalDays);
+                OldDay += DateAndTime.Day - OldDay;
+                if (OldHour > DateTime.DaysInMonth(DateAndTime.Year, DateAndTime.Month)) { OldDay = 1; }
                 Day?.Invoke();
                 if (DateAndTime.DayOfWeek == DayOfWeek.Sunday)
                 {
                     Week?.Invoke();
                 }
+            }
+            if (DateAndTime.Month > OldDateAndTime.Month & DateAndTime.Year == OldDateAndTime.Year || DateAndTime.Month < OldDateAndTime.Month & DateAndTime.Year > OldDateAndTime.Year)
+            {
+                OldDateAndTime = OldDateAndTime.AddMonths(1);
+                Month?.Invoke();
+            }
+            if (DateAndTime.Year > OldDateAndTime.Year)
+            {
+                OldDateAndTime = OldDateAndTime.AddYears(1);
+                Year?.Invoke();
             }
         }
     }
